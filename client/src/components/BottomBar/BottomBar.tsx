7bdaaa96992337c1a2c972d94f198686d65f75da
Manuel Fideles <manuelfideles77@gmail.com>
2023-04-10 11:49:08 +0100

    Revert "CS-{0,1,2,3,14,15,16,18,19,20}: Data processing and basic SPA scaffolding. Implemented login, sign-up form and auth logic-ish"
    This reverts commit a2e108b14f78ab8efb586a3ccde1477d03086a53.

--- client/src/components/BottomBar/BottomBar.tsx
+++
@@ -1,9 +0,0 @@
-import { Box } from '@chakra-ui/react'
-
-export const BottomBar = () => {
-	return (
-		<Box>
-			BottomBar
-		</Box>
-	)
-}
--- client/src/pages/Dashboard.tsx
+++
@@ -1,7 +0,0 @@
-import { Sidebar } from '../components/Sidebar'
-
-export const Dashboard = () => {
-	return (
-		<Sidebar />
-	)
-}
