05bec55f40ad32863a065bb8218c1d0ff3ec4468
Fideles <fideles@Fideless-Air.lan>
2023-06-06 19:52:08 +0100

    CS-24: major refactors + bottom bar styling

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,11 +1,13 @@
 import { Sidebar } from '../components/Sidebar';
 import { MapBox } from '../components/MapBox';
+import { BottomBar } from '../components/BottomBar';
 
 export const Dashboard = () => {
   return (
     <>
       <Sidebar />
       <MapBox />
+      <BottomBar />
     </>
   );
 };
