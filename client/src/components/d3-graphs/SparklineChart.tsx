bb49bfa25e52d8f237503ba1c2a3518a9c693d24
manelfideles <manuelfideles77@gmail.com>
2023-07-28 17:32:08 +0100

    Finished SparklineChart

--- client/src/components/d3-graphs/SparklineChart.tsx
+++ client/src/components/d3-graphs/SparklineChart.tsx
@@ -3,7 +3,7 @@ import { LineSeries, FlexibleXYPlot } from 'react-vis';
 import { useLocalStorage } from 'usehooks-ts';
 import { User } from '../../utils/types';
 import { getDefaultUserValues } from '../../utils/mockData';
-// import { useGraphSlider } from '../../hooks/useGraphSlider';
+import { useGraphSlider } from '../../hooks/useGraphSlider';
 
 interface SparklineChartProps {
   data: any[];
@@ -22,7 +22,7 @@ export const SparklineChart = ({
     },
     _setSettings,
   ] = useLocalStorage<User>('settings', getDefaultUserValues());
-  // const { mode, sliderRange, sliderValues } = useGraphSlider();
+  const { sliderValues } = useGraphSlider();
   const dataByVariable = Object.entries(groupBy(data, 'variable_id')).map(
     ([variable_id, variable_values]) => ({
       variable_id,
@@ -32,19 +32,36 @@ export const SparklineChart = ({
       })),
     })
   );
+  const filteredData = Object.entries(groupBy(data, 'variable_id')).map(
+    ([variable_id, variable_values]) => ({
+      variable_id,
+      values: variable_values
+        .filter(
+          ({ altitude }) =>
+            altitude >= sliderValues[0] && altitude <= sliderValues[1]
+        )
+        .map(({ altitude, average_value }) => ({
+          x: altitude,
+          y: average_value,
+        })),
+    })
+  );
+
   return (
     <FlexibleXYPlot
       height={height}
       width={width}
       margin={{ left: 0, top: 5, bottom: 0, right: 0 }}
     >
-      {dataByVariable.map(({ variable_id, values }) => (
+      {dataByVariable.map(({ values }) => (
+        <LineSeries data={values} color='lightgray' opacity={0.5} />
+      ))}
+      {filteredData.map(({ variable_id, values }) => (
         <LineSeries
+          data={values}
           color={
             variables.filter(({ id }) => id === Number(variable_id))[0].color
           }
-          curve='curveMonotoneX'
-          data={values}
         />
       ))}
     </FlexibleXYPlot>
